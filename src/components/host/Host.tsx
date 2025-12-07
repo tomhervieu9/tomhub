1d63330a28c54ce2345b2fdcb09aec9e0e531df3
tom <tom.hervieu@gmail.com>
2025-12-07 17:24:51 -0500

    fix(host): fix host image growing too large in portrait

--- src/components/host/Host.tsx
+++ src/components/host/Host.tsx
@@ -3,24 +3,38 @@ import Image from "next/image";
 import { ThoughtBubble } from "./ThoughtBubble";
 
 export const Host = (props: { className: string }) => {
+  // TODO: improve the sizing methodology for the next/image component.
+
   return (
-    <div className={cn("flex-2 min-w-0 min-h-0", props.className)}>
-      <div className="flex md:flex-col flex-row w-full h-full">
-        <div className="relative flex-1">
+    <div
+      id="host-container"
+      className={cn("flex-2 min-w-0 min-h-0", props.className)}
+    >
+      <div
+        id="host-inner-container"
+        className="flex md:flex-col flex-row w-full h-full "
+      >
+        <div
+          id="thought-bubble-container"
+          className="relative flex-10 flex items-end justify-center"
+        >
           <ThoughtBubble />
         </div>
-        <div className="flex-1 flex items-end justify-center">
-          <div id="image-container" className="relative w-fit h-fit">
-            <Image
-              src="/images/portfolio-shot.png"
-              alt="Profile Picture"
-              priority
-              width={0}
-              height={0}
-              sizes="100%"
-              className="w-fit h-fit"
-            />
-          </div>
+        <div id="host-gap" className="flex-1" />
+        <div
+          id="host-image-container"
+          className="flex items-end justify-center"
+        >
+          <Image
+            id="host-image"
+            src="/images/portfolio-shot.png"
+            alt="Profile Picture"
+            priority
+            width={0}
+            height={0}
+            sizes="100%"
+            className="w-auto h-auto max-h-full"
+          />
         </div>
       </div>
     </div>
